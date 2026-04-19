00965296a8f30fce749e217898814d6f247dfe12
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-19 20:20:28 -0300

    feat: create Card.tsx component

---
+++ weather-project/src/components/cards/Card.tsx
@@ -0,0 +1,16 @@
+import React, { type ReactNode } from 'react'
+
+type CardProps = {
+    children: ReactNode,
+    title: string,
+
+}
+
+export default function Card({children, title}: CardProps) {
+    return (
+        <div className='p-4 rounded-xl bg-zinc-900 shadow-md flex flex-col gap-4'>
+            <h2 className='text-2xl font-semibold'>{title}</h2>
+            <div>{children}</div>
+        </div>
+    )
+}
