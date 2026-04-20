586eca8b8809f92a0cae01370e1f233b85803c2c
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-20 16:43:37 -0300

    feat: create Map.tsx using using React Leaflet

---
+++ weather-project/src/components/Map.tsx
@@ -0,0 +1,33 @@
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+// type MapProps = {}
+
+export default function Map() {
+  return (
+    <MapContainer
+     center={[50, 50]}
+     zoom={5}
+     style={{width: '1000px', height: '500px'}}
+    >     
+    <MapClick />
+    <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[50, 50]} />
+    </MapContainer>
+
+  )
+}
+
+function MapClick() {
+    const map = useMap();
+
+    map.on("click", (e) => {
+        const {lat, lng} = e.latlng
+        map.panTo([lat, lng])
+    })
+
+    return null 
+}
