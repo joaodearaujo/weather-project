238374fe48310aad75ed6d2045543488599f4d27
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-19 21:04:53 -0300

    feat: create WeatherIcon.tsx

---
+++ weather-project/src/components/cards/WeatherIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type WeatherIconProps = {
+    src: string,
+}
+
+export default function WeatherIcon({src}: WeatherIconProps) {
+  return (
+    <img
+        className='size-8' 
+        src={`https://openweathermap.org/payload/api/media/file/${src}.png}`} 
+        alt="Weather Icon" 
+    />
+  )
+}
+
+
